2cc5c412793350000d17dde51d8d4359fdecd179
Habibayasser49 <habelsadek@gmail.com>
2026-08-25 11:53:12 +0300

    fix: support database connection on Vercel

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const cors = require('cors');
 
+const connectDB = require('./config/db')
+
 const swaggerUi = require('swagger-ui-express');
 
 const mongoose = require('mongoose');
--- config/db.js
+++ config/db.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+let connectionPromise = null; 
 
-        console.log(`MongoDB connected : ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`MongoDB connection failed : ${error.message}`);
-        process.exit(1);
+const connectDB = async () => {
+    if (mongoose.connection.readyState === 1) {
+        return;
     }
+
+    if (!connectionPromise) {
+        const connectionPromise = mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000
+        })
+
+            .then((conn) => {
+                console.log(`MongoDB connected : ${conn.connection.host}`);
+            })
+
+            .catch ((error) => {
+                connectionPromise = null;
+                console.error(`MongoDB connection failed : ${error.message}`);
+                throw error;
+        });
+    }
+
+    await connectionPromise;
 };
 
 module.exports = connectDB;
